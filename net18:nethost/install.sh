2908f19d85c1057801dfc267648406d782a7e60e
isx53320159 <isx53320159@i12.informatica.escoladeltreball.org>
2019-03-06 12:22:03 +0100

    proba

--- net18:nethost/install.sh
+++ net18:nethost/install.sh
@@ -1,1 +1,11 @@
 #! /bin/bash
+echo "hola" > /var/www/html/index.html
+useradd -g users user01
+useradd -g users user02
+useradd -g users user03
+
+echo "user01" | passwd --stdin user01
+echo "user02" | passwd --stdin user02
+echo "user03" | passwd --stdin user03
+
+
