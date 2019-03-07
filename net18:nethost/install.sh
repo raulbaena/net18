5628f0b8eea6215103a2f297a6e0e26c62707313
isx53320159 <isx53320159@i12.informatica.escoladeltreball.org>
2019-03-07 08:22:43 +0100

    cosa

--- net18:nethost/install.sh
+++ net18:nethost/install.sh
@@ -3,9 +3,18 @@ echo "hola" > /var/www/html/index.html
 useradd -g users user01
 useradd -g users user02
 useradd -g users user03
-
 echo "user01" | passwd --stdin user01
 echo "user02" | passwd --stdin user02
 echo "user03" | passwd --stdin user03
-
-
+#Fer un tar i el expandiran amb add
+cp /opt/docker/chargen-dgram  /etc/xinetd.d/
+cp /opt/docker/chargen-stream /etc/xinetd.d/
+cp /opt/docker/daytime-bis    /etc/xinetd.d/
+cp /opt/docker/daytime-dgram  /etc/xinetd.d/
+cp /opt/docker/daytime-stream /etc/xinetd.d/
+cp /opt/docker/Dockerfile     /etc/xinetd.d/
+cp /opt/docker/echo-bis       /etc/xinetd.d/
+cp /opt/docker/echo-dgram     /etc/xinetd.d/
+cp /opt/docker/echo-stream    /etc/xinetd.d/
+cp /opt/docker/httpd-bis      /etc/xinetd.d/
+cp /opt/docker/httpd-tris     /etc/xinetd.d/
